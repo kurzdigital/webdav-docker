ebdbbebf86c99929422c417f3aa31a41301c0b3b
Nikolai Prokoschenko <nikolai.prokoschenko@kurzdigital.com>
2018-09-06 09:10:46 +0200

    Use apache2ctl instead of apache2

--- webdav/run.sh
+++ webdav/run.sh
@@ -4,4 +4,4 @@ htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
 chown root:www-data /etc/apache2/webdav.password
 chmod 640 /etc/apache2/webdav.password
 
-apache2 -D FOREGROUND
+apache2ctl -D FOREGROUND
